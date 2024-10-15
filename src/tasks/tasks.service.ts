28f5b773f92508e7ff6dc6b94614b2734c1117ae
Tony Herbet Le Faucheur <herbet.le.faucheur.tony@gmail.com>
2024-10-15 20:22:26 +0200

    section 2.26 challenge

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -32,4 +32,13 @@ export class TasksService {
   deleteTask(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
+
+  updateTaskStatus(id: string, status: TaskStatus): void {
+    this.tasks = this.tasks.map((task) => {
+      if (task.id === id) {
+        task.status = status;
+      }
+      return task;
+    });
+  }
 }
